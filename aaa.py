43ea15abbff767126704f273dccd7dc06c4ad364
santiurru <santiurru@gmail.com>
2022-05-05 23:05:37 -0300

    lista de palabras de 5

--- aaa.py
+++ aaa.py
@@ -1,1 +1,9 @@
-
+with open("palabras5.txt", "w") as g:
+    with open("listado.txt", "r") as f:
+        for line in f:
+            f_datos = f.readline()
+            #print(f.readline)
+            if len(f_datos) == 6:
+                print("5")
+                g.write(f_datos)
+print("hecho")
