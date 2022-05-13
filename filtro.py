940678914f2c229aa9729f71a80c453d8daa4173
santiurru <santiurru@gmail.com>
2022-05-12 21:05:01 -0300

    v2.3

--- filtro.py
+++ filtro.py
@@ -2,16 +2,16 @@ import random
 
 print(">>>FILTRANDO PALABRAS DE 5 LETRAS")
 
-cant_5s=0
-lista5=[]
+cant_5s=0                                   #numero de palabras de 5 letras
+lista5=[]                                   #Lista de palabras
 
-with open("palabras5.txt", "w") as g:
+with open("palabras5.txt", "w") as g:       #para guardar las palabras en un archivo .txt aparte
     with open("listado.txt", "r", encoding="utf8") as f:
         for line in range (1290):
             f_datos = f.readline()
             if len(f_datos) == 6:
                 cant_5s=cant_5s+1
-                g.write(f_datos)
+                g.write(f_datos)            #para guardar las palabras en un archivo .txt aparte
                 lista5.append(f_datos.rstrip())
                 #print(f_datos)
 
