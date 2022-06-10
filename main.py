d8547aa0aa7a0f8f0b578159e2da6b64f5006892
santiurru4 <santiurru4@gmail.com>
2022-06-10 13:37:38 -0300

    aaa

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ if __name__=="__main__":
     from colorama import Fore, Back, Style
     colorama.init(autoreset=True)
     #----------------------------------------|ETAPA 1|------------------------------------------
-    print(">>>FILTRANDO PALABRAS DE 5 LETRAS")
+    #print(">>>FILTRANDO PALABRAS DE 5 LETRAS")
 
     cant_5s=0                                   #numero de palabras de 5 letras
     lista5=[]                                   #Lista de palabras
@@ -17,8 +17,8 @@ if __name__=="__main__":
                     g.write(f_datos)            #para guardar las palabras en un archivo .txt aparte
                     lista5.append(f_datos.rstrip())
 
-    print(">>>OPERACION COMPLETADA CON EXITO")
-    print(">>>SE ENCONTRARON", cant_5s, "PALABRAS DE 5 LETRAS")
+    #print(">>>OPERACION COMPLETADA CON EXITO")
+    #print(">>>SE ENCONTRARON", cant_5s, "PALABRAS DE 5 LETRAS")
     #-----------------------------------------|ETAPA 2|------------------------------------------
 
     def pregunta(q):                #revisa errores en la palabra introducida
@@ -42,14 +42,14 @@ if __name__=="__main__":
             y_restantes=cantidad_y_i-cantidad_y_dev
             if x==y:
                 devolucion.append(Fore.GREEN+y)#Verde
-                y_restantes=y_restantes-1
+                #y_restantes=y_restantes-1
                 if y_restantes < 0:
                     sss=devolucion.index(Fore.YELLOW+y)
                     devolucion[sss] = Fore.WHITE+y   #elimina amarillo
             elif y in a:
                 if y_restantes>0:
                     devolucion.append(Fore.YELLOW+y)#Amarillo
-                    y_restantes=y_restantes-1
+                    #y_restantes=y_restantes-1
                 else:
                     devolucion.append(Fore.WHITE+y)#Blanco
             else:
@@ -65,7 +65,7 @@ if __name__=="__main__":
             No use caracteres especiales.      """, Back.RESET)
     intentos=5
     solucion = random.choice(lista5)
-    #print("La palabra aleatoria es", solucion)     #borrar el "#" para probar
+    print("La palabra aleatoria es", solucion)     #borrar el "#" para probar
     palabra=input(Back.CYAN+"Introduzca una palabra:\n"+Back.RESET)
     while intentos>0:
         if palabra!=solucion:
@@ -77,8 +77,10 @@ if __name__=="__main__":
             if palabra!=solucion:
                 devolucion=[]
                 print(Fore.RED+"le quedan ",str(intentos),"intentos")
-                palabra=input(Back.CYAN+"Siguiente intento:\n"+Back.RESET)
                 intentos=intentos-1
+                if intentos!=0:
+                    palabra=input(Back.CYAN+"Siguiente intento:\n"+Back.RESET)
+                
         if palabra==solucion:
             print(Back.GREEN+solucion)
             print("¡Ganaste!")
